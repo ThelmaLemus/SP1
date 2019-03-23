5d8ca21c76f47b5719730c5b404dc172e6cc7a74
ThelmaLemus <thelma.lemus@galileo.edu>
2019-03-23 15:43:46 -0600

    Add files via upload

---
+++ src/app/registro2/registro2.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { Registro2Page } from './registro2.page';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Registro2Page
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [Registro2Page]
+})
+export class Registro2PageModule {}
